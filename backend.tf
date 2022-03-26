b7d35f15be1be094b534f1427a90cd79efd95be7
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2022-03-26 19:38:23 +0100

    first commit

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.0.2"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "paulaviatrix_iac"
+    workspaces {
+      name = "ace-iac-day-zero"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,68 @@
+variable "username" {
+  default = "admin"
+}
+
+variable "password" {
+}
+
+variable "controller_ip" {
+}
+
+variable "ace_password" {
+}
+
+variable "aws_account_name" {
+}
+
+variable "aws_transit_instance_size" {
+  default = "t3.micro"
+}
+
+variable "aws_transit1_region" {
+  default = "us-east-2"
+}
+
+variable "aws_transit1_name" {
+  default = "avx-trns1"
+}
+
+variable "aws_transit1_cidr" {
+  default = "10.21.0.0/16"
+}
+
+variable "aws_spoke_instance_size" {
+  default = "t3.micro"
+}
+
+variable "aws_spoke1_region" {
+  default = "us-east-2"
+}
+
+variable "aws_spoke1_name" {
+  default = "ace-iac-spoke1"
+}
+
+variable "aws_spoke1_cidr" {
+  default = "10.1.0.0/16"
+}
+
+variable "aws_spoke2_region" {
+  default = "us-east-2"
+}
+
+variable "aws_spoke2_name" {
+  default = "ace-iac-spoke2"
+}
+
+variable "aws_spoke2_cidr" {
+  default = "10.2.0.0/16"
+}
+variable "aws_test_instance_size" {
+  default = "t2.micro"
+}
+
+
+variable "ha_enabled" {
+  type    = bool
+  default = false
+}
