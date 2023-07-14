1267e2e2a01283a6e885e9719a68c78cf0331b81
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2023-07-14 18:26:15 +0200

    spokes

--- spokes.tf
+++ spokes.tf
@@ -27,4 +27,17 @@ module "spoke_aws_2" {
   transit_gw      = "avx-eu-central-1-transit"
 }
 
-  
+
+module "spoke_az_1" {
+  source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
+  version = "1.6.2"
+
+  cloud           = "AZURE"
+  account         = var.az_account_name
+  region          = var.az_spoke1_region
+  name            = var.az_spoke1_name
+  cidr            = var.az_spoke1_cidr
+  instance_size   = var.az_spoke_instance_size
+  ha_gw           = var.ha_enabled
+  transit_gw      = "avx-west-europe-transit"
+}
--- variables.tf
+++ variables.tf
@@ -14,6 +14,8 @@ variable "aws_account_name" {
   default = "AWS_Paul"
 }
 
+
+
 variable "mc_transit_region" {
   default = "eu-central-1"
 }
@@ -28,13 +30,25 @@ variable "mc_transit_cidr" {
 
 
 
-
-
-
 variable "aws_transit_instance_size" {
   default = "t3.micro"
 }
 
+
+
+
+variable "ha_enabled" {
+  type    = bool
+  default = false
+}
+
+
+
+
+
+
+
+# AWS SPOKES
 
 variable "aws_spoke_instance_size" {
   default = "t3.micro"
@@ -63,12 +77,76 @@ variable "aws_spoke2_name" {
 variable "aws_spoke2_cidr" {
   default = "10.2.0.0/16"
 }
+
+
+
+# AZURE SPOKES
+
+variable "az_spoke_instance_size" {
+  default = "Standard_B1ms"
+}
+
+variable "az_spoke1_region" {
+  default = "West Europe"
+}
+
+variable "az_spoke1_name" {
+  default = "az-r1-spoke1"
+}
+
+variable "az_spoke1_cidr" {
+  default = "10.110.0.0/16"
+}
+
+variable "az_spoke2_region" {
+  default = "UK South"
+}
+
+variable "az_spoke2_name" {
+  default = "az-r2-spoke1"
+}
+
+variable "az_spoke2_cidr" {
+  default = "10.111.0.0/16"
+}
+
+
+
+
+# GCP SPOKES
+
+variable "gcp_spoke_instance_size" {
+  default = "n1-standard-1"
+}
+
+variable "gcp_spoke1_region" {
+  default = "europe-west1"
+}
+
+variable "gcp_spoke1_name" {
+  default = "gcp-spoke1"
+}
+
+variable "gcp_spoke1_cidr" {
+  default = "10.120.0.0/16"
+}
+
+variable "gcp_spoke2_region" {
+  default = "europe-west1"
+}
+
+variable "gcp_spoke2_name" {
+  default = "gcp-spoke2"
+}
+
+variable "gcp_spoke2_cidr" {
+  default = "10.121.0.0/16"
+}
+
+
+
+
 variable "aws_test_instance_size" {
   default = "t2.micro"
 }
 
-
-variable "ha_enabled" {
-  type    = bool
-  default = true
-}
